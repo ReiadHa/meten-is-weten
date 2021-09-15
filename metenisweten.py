d2c6abc4840e0ac9c7c2c5de6a57dadeb0fe4156
Riad Hak <99054420@mydavinci.nl>
2021-09-15 11:34:53 +0200

    meten is weten

---
+++ metenisweten.py
@@ -0,0 +1,20 @@
+a = int(input('vul het getal hier in voor A '))
+b = int(input('Vul het getal hier in voor B '))
+print(type(a))
+if a > b:
+    print('A is het grootste getal ' + str(a))
+    
+max = a
+
+
+
+
+
+
+
+
+
+
+
+
+
