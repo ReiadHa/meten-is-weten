c03e13474841d42bcf645f883cd7759e02ae0a7a
Riad Hak <99054420@mydavinci.nl>
2021-09-17 10:19:43 +0200

    meten is weten

--- metenisweten.py
+++ metenisweten.py
@@ -1,10 +1,22 @@
 a = int(input('vul het getal hier in voor A '))
 b = int(input('Vul het getal hier in voor B '))
-print(type(a))
-if a > b:
-    print('A is het grootste getal ' + str(a))
-    
 max = a
+min = b
+
+if a > b:
+    print('A is het grootste getal : ' + str(max))
+
+elif a < b :
+    print('A is het kleiner getal : ' + str(max))
+
+if b > a:
+    print('B is het grootste getal : ' + str(min))
+
+elif  b < a :
+    print('B is het kleinste getal : ' + str(min))
+
+else :
+    print('A en B zijn even groot : ' + str(max) + ' = ' + str(min))
 
 
 
